7c5dde0a20ed89323424bd4d85b2f77aad674391
Kevin Martensson <kevinmartensson@gmail.com>
2016-04-11 21:13:16 +0200

    Use `only-shallow` instead of `JSON.stringify()`
    Because `JSON.stringify()` requires to objects to be structured exactly the same which is kinda flaky.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
+var deepEqual = require('only-shallow');
+
 module.exports = function (input, values) {
 	if (!Array.isArray(values)) {
 		throw new TypeError('Expected an array');
 	}
 
 	for (var i = 0; i < values.length; i++) {
-		if (input === values[i] || JSON.stringify(input) === JSON.stringify(values[i])) {
+		if (input === values[i] || deepEqual(input, values[i])) {
 			return true;
 		}
 	}
